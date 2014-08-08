b7a99e709712ca096d67dee41c5b10a55ae1f4f1
TJ Horner <rowenrocks@gmail.com>
2014-08-08 16:34:03 -0700

    first commit with shit

---
+++ js/main.js
@@ -0,0 +1,129 @@
+// from http://stackoverflow.com/a/2419877/2449940
+jQuery.fn.outerHTML = function(s) {
+	return s
+		? this.before(s).remove()
+		: jQuery("<p>").append(this.eq(0).clone()).html();
+};
+
+window.Site = (function(){
+	// from http://davidwalsh.name/vendor-prefix
+	var scrollLast = 0;
+	$toolbar = $('.toolbar');
+	var s = window.getComputedStyle(document.documentElement, '');
+	var prefix = (Array.prototype.slice
+		.call(s)
+		.join('') 
+		.match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+	)[1];
+	// god damn robots
+	var emCoded = 'Z2Vla3lnYW1lcjE0QGlxdWVzdHJpYS5uZXQ=';
+	var number1 = Math.floor(Math.random()*10+1);
+	var number2 = Math.floor(Math.random()*20+1);
+	var Site = {
+		Templates: {
+			// mustache.js templates
+			Block: (function(){
+				$el = $(document.createElement('div'));
+				$el.addClass('block')
+					.attr('style', 'background: {{background}}')
+					.append('<h1>{{title}}</h1>')
+					.append('<div class="block-content"><span class="indent"></span>{{content}}</div>');
+				return $el[0].outerHTML;
+			}()),
+
+			ContactIcon: (function(){
+				$el = $(document.createElement('a'));
+				$el.append('<i title="{{title}}" class="fa fa-{{icon}}"></i>')
+					.attr('href', '{{href}}');
+				return $el[0].outerHTML;
+			}())
+		},
+
+		// no sass, no problem
+		Prefixify: function(styleToPrefix){
+			return "-"+prefix+"-"+styleToPrefix;
+		},
+
+		ProveHuman: function(){
+			var answer = prompt("Quick human check! What's "+number1+" + "+number2+"?");
+			if(answer === (number1+number2).toString()){
+				prompt("Hello there, human! Here's my email.", atob(emCoded));
+			}else{
+				if(answer !== null)
+					Site.ProveHuman();
+			}
+		}
+	};
+
+	var blocks = [
+		{
+			title: "here is a story",
+			content: "Developers developers developers developers developers developers developers developers developers developers developers programming Whoo! winning powerpoint presentation bing. Windows Whoo! outlook hotmail i have never honestly thrown a chair in my life. Google is not a real company linux is a cancer programming windows. Winning i have never honestly thrown a chair in my life hotmail there's no chance that the iPhone is going to get any significant market share microsoft word i don't know what a monopoly is DEVELOPERS Whoo!. Leadership linux is not in the public domain windows will be everywhere Whoo! google is not a real company there's no chance that the iPhone is going to get any significant market share."
+		},
+		{
+			title: "no",
+			content: "Developers developers developers developers developers developers developers developers developers developers developers programming Whoo! winning powerpoint presentation bing. Windows Whoo! outlook hotmail i have never honestly thrown a chair in my life. Google is not a real company linux is a cancer programming windows. Winning i have never honestly thrown a chair in my life hotmail there's no chance that the iPhone is going to get any significant market share microsoft word i don't know what a monopoly is DEVELOPERS Whoo!. Leadership linux is not in the public domain windows will be everywhere Whoo! google is not a real company there's no chance that the iPhone is going to get any significant market share."
+		},
+		{
+			title: "teST",
+			content: "Developers developers developers developers developers developers developers developers developers developers developers programming Whoo! winning powerpoint presentation bing. Windows Whoo! outlook hotmail i have never honestly thrown a chair in my life. Google is not a real company linux is a cancer programming windows. Winning i have never honestly thrown a chair in my life hotmail there's no chance that the iPhone is going to get any significant market share microsoft word i don't know what a monopoly is DEVELOPERS Whoo!. Leadership linux is not in the public domain windows will be everywhere Whoo! google is not a real company there's no chance that the iPhone is going to get any significant market share."
+		}
+	];
+
+	$.each(blocks, function(o,i){
+		var color = Please.make_color();
+		if(o === 0)
+			$('body').css('background', color);
+		i.background = color;
+		$('.content').append(Mustache.render(Site.Templates.Block, i));
+	});
+
+	var contacts = [
+		{
+			title: "GitHub",
+			icon: "github",
+			href: "https://github.com/geekygamer14"
+		},
+		{
+			title: "Email",
+			icon: "envelope",
+			href: "#",
+			listener: Site.ProveHuman
+		},
+		{
+			title: "Steam",
+			icon: "steam",
+			href: "http://steamcommunity.com/id/geekygamer14"
+		},
+		{
+			title: "Google+",
+			icon: "google-plus",
+			href: "http://google.com/+TJHorner"
+		},
+		{
+			title: "Facebook",
+			icon: "facebook",
+			href: "http://facebook.com/geekygamer14"
+		}
+	]
+
+	$.each(contacts, function(o,i){
+		$el = $(Mustache.render(Site.Templates.ContactIcon, i));
+		if(i.listener){
+			$el.click(i.listener);
+		}
+		$('.toolbar').append($el);
+		$el.after(' ');
+	});
+
+	$(document).scroll(function(){
+		if(scrollLast < window.scrollY){
+			$toolbar.addClass('hidden');
+		}else{
+			$toolbar.removeClass('hidden');
+		}
+		scrollLast = window.scrollY;
+	});
+
+	return Site;
+}());
