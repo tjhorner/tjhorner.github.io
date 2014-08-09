8e37a9cbfedeeba91d9bc715e286346bf39d5840
TJ Horner <rowenrocks@gmail.com>
2014-08-08 21:53:27 -0700

    [good commit message]

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@ jQuery.fn.outerHTML = function(s) {
 window.Site = (function(){
 	// from http://davidwalsh.name/vendor-prefix
 	var scrollLast = 0;
+	// improve query times
 	$toolbar = $('.toolbar');
 	$sidebar = $('.sidebar');
+	$footer = $('.footer');
 	var s = window.getComputedStyle(document.documentElement, '');
 	var prefix = (Array.prototype.slice
 		.call(s)
@@ -18,8 +20,21 @@ window.Site = (function(){
 	)[1];
 	// god damn robots
 	var emCoded = 'Z2Vla3lnYW1lcjE0QGlxdWVzdHJpYS5uZXQ=';
+	var ballmeripsum = "Developers developers developers developers developers developers developers developers developers developers developers programming Whoo! winning powerpoint presentation bing. Windows Whoo! outlook hotmail i have never honestly thrown a chair in my life. Google is not a real company linux is a cancer programming windows. Winning i have never honestly thrown a chair in my life hotmail there's no chance that the iPhone is going to get any significant market share microsoft word i don't know what a monopoly is DEVELOPERS Whoo!. Leadership linux is not in the public domain windows will be everywhere Whoo! google is not a real company there's no chance that the iPhone is going to get any significant market share.";
 	var number1 = Math.floor(Math.random()*10+1);
 	var number2 = Math.floor(Math.random()*20+1);
+	var names = ["a programmer", "GeekyGamer14"];
+
+	var ProveHuman = function(){
+		var answer = prompt("Quick human check! What's "+number1+" + "+number2+"?");
+		if(answer === (number1+number2).toString()){
+			prompt("Hello there, human! Here's my email.", atob(emCoded));
+		}else{
+			if(answer !== null)
+				Site.ProveHuman();
+		}
+	}
+
 	var Site = {
 		Templates: {
 			// mustache.js templates
@@ -29,7 +44,7 @@ window.Site = (function(){
 					.attr('id', '{{id}}')
 					.attr('style', 'background: {{background}}')
 					.append('<h1><i class="fa fa-{{icon}}"></i> {{{title}}}</h1>')
-					.append('<div class="block-content"><span class="indent"></span>{{{content}}}</div>');
+					.append('<div class="block-content">{{{content}}}</div>');
 				return $el[0].outerHTML;
 			}()),
 
@@ -53,36 +68,31 @@ window.Site = (function(){
 		// no sass, no problem
 		Prefixify: function(styleToPrefix){
 			return "-"+prefix+"-"+styleToPrefix;
-		},
-
-		ProveHuman: function(){
-			var answer = prompt("Quick human check! What's "+number1+" + "+number2+"?");
-			if(answer === (number1+number2).toString()){
-				prompt("Hello there, human! Here's my email.", atob(emCoded));
-			}else{
-				if(answer !== null)
-					Site.ProveHuman();
-			}
 		}
 	};
 
 	var blocks = [
 		{
-			title: "About Me",
+			title: "Welcome",
 			icon: "user",
-			content: "Developers developers developers developers developers developers developers developers developers developers developers programming Whoo! winning powerpoint presentation bing. Windows Whoo! outlook hotmail i have never honestly thrown a chair in my life. Google is not a real company linux is a cancer programming windows. Winning i have never honestly thrown a chair in my life hotmail there's no chance that the iPhone is going to get any significant market share microsoft word i don't know what a monopoly is DEVELOPERS Whoo!. Leadership linux is not in the public domain windows will be everywhere Whoo! google is not a real company there's no chance that the iPhone is going to get any significant market share.",
+			content: (function(){
+				$div = $('<div></div>');
+				$div.html("<h1>Hi, I'm <span id=\"name-fill\">TJ</span>.</h1><h3>Here's some stuff I do.</h3>")
+					.addClass('center fill');
+				return $div[0].outerHTML;
+			}()),
 			id: "about"
 		},
 		{
 			title: "Projects",
 			icon: "code",
-			content: "Developers <a href='developers'>developers</a> developers developers developers developers developers developers developers developers developers programming Whoo! winning powerpoint presentation bing. Windows Whoo! outlook hotmail i have never honestly thrown a chair in my life. Google is not a real company linux is a cancer programming windows. Winning i have never honestly thrown a chair in my life hotmail there's no chance that the iPhone is going to get any significant market share microsoft word i don't know what a monopoly is DEVELOPERS Whoo!. Leadership linux is not in the public domain windows will be everywhere Whoo! google is not a real company there's no chance that the iPhone is going to get any significant market share.",
+			content: ballmeripsum,
 			id: "projects"
 		},
 		{
 			title: "Something",
 			icon: "bomb",
-			content: "Developers developers developers developers developers developers developers developers developers developers developers programming Whoo! winning powerpoint presentation bing. Windows Whoo! outlook hotmail i have never honestly thrown a chair in my life. Google is not a real company linux is a cancer programming windows. Winning i have never honestly thrown a chair in my life hotmail there's no chance that the iPhone is going to get any significant market share microsoft word i don't know what a monopoly is DEVELOPERS Whoo!. Leadership linux is not in the public domain windows will be everywhere Whoo! google is not a real company there's no chance that the iPhone is going to get any significant market share.",
+			content: ballmeripsum,
 			id: "something"
 		}
 	];
@@ -111,7 +121,7 @@ window.Site = (function(){
 			title: "Email",
 			icon: "envelope",
 			href: "#",
-			listener: Site.ProveHuman
+			listener: ProveHuman
 		},
 		{
 			title: "Steam",
@@ -147,9 +157,11 @@ window.Site = (function(){
 	$(document).scroll(function(){
 		if(scrollLast < window.scrollY){
 			$toolbar.addClass('hidden');
+			$footer.addClass('hidden');
 			$sidebar.addClass('toolbar-hidden');
 		}else{
 			$toolbar.removeClass('hidden');
+			$footer.removeClass('hidden');
 			$sidebar.removeClass('toolbar-hidden');
 		}
 		scrollLast = window.scrollY;
