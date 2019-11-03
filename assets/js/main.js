b6e92c622833627520ed403e0971be39c99eb5b6
TJ Horner <me@tjhorner.com>
2019-11-02 23:17:54 -0400

    git it done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
-var load = data => {
-  $("#song-name").text(data.lastfm.track)
-  $("#song-artist").text(data.lastfm.artist)
+// old school cool
+window.onload = async () => {
+  const res = await fetch("https://whereis.tjhorner.nyc/api/v1/location")
+  const location = await res.json()
+  if(!location.search_query) return
+  current_location.innerText = location.search_query
+  current_location.title = "Yes, this is live!"
 }
