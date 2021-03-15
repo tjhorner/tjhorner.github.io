4afe8a90c731fe17892b6ef13a228de3da2b94c4
TJ Horner <me@tjhorner.com>
2021-03-15 01:45:53 -0400

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,30 @@
-// old school cool
-window.onload = async () => {
-  const res = await fetch("https://whereis.tjhorner.nyc/api/v1/location")
-  const location = await res.json()
-  if(!location.search_query) return
-  current_location.innerText = location.search_query
-  current_location.title = "Yes, this is live!"
+const load = async () => {
+  const query = "{ location { searchQuery } nowPlaying { isPlaying track { title link artists { name } } album { imageUrl } } }"
+  
+  const res = await fetch("https://api.horner.tj", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query
+    })
+  })
+  
+  const { data } = await res.json()
+  
+  if (data.location && data.location.searchQuery) {
+    current_location.innerText = data.location.searchQuery
+    current_location.title = "Yes, this is live!"
+  }
+
+  if (data.nowPlaying) {
+    album_art.src = data.nowPlaying.album.imageUrl
+    track_title.innerText = track_title.title = data.nowPlaying.track.title
+    track_artist.innerText = track_artist.title = data.nowPlaying.track.artists.map(a => a.name).join(", ")
+    now_playing.className += " animated fadeInLeft active"
+    now_playing.href = data.nowPlaying.track.link
+  }
 }
+
+window.onload = load
